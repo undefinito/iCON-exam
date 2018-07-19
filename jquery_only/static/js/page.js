220024200f45114deb99ef2d162e5cc8854a5898
undefinito <hindakosned@gmail.com>
2018-07-19 13:23:44 +0800

    card html loading
    user card html in horizontal list being loaded and saved for faster
    rendering
    need to add handling when error in getting the html fragments

--- jquery_only/static/js/page.js
+++ jquery_only/static/js/page.js
@@ -1,5 +1,6 @@
 var HOST = "https://reqres.in"
 var $li = null
+var $card = null
 
 function formatToLi(data) {
 
@@ -10,9 +11,6 @@ function formatToLi(data) {
 
   var list_html = ''
   for (var i = 0; i < data.length; i++) {
-    // list_html += '<li class="list-group-item">'
-    // list_html += data[i].name
-    // list_html += '</li>'
 
     var $temp = $li.clone()
     $temp.find('[data-head]').text(data[i].name)
@@ -25,13 +23,24 @@ function formatToLi(data) {
   return list_html
 }
 
-$(document).ready(function() {
+// GET list item html and place into variable
+$.get('html/list_item.html')
+  .done(function(result) {
+    $li = $(result)
+  })
+  .fail(function() {
+    // TODO handler of error
+  })
 
-  // GET list item html and place into variable
-  $.get('html/list_item.html')
-    .done(function(result) {
-      $li = $(result)
-    })
+$.get('html/user_card.html')
+  .done(function(result) {
+    $card = $(result)
+  })
+  .fail(function() {
+    // TODO handler of error
+  })
+
+$(document).ready(function() {
 
   // GET users (for horizontal list)
   $.ajax({
@@ -49,9 +58,6 @@ $(document).ready(function() {
     beforeSend: function() {
       $('#list').children('.loading')
           .removeClass('d-none')
-    },
-    data: {
-      page: 1
     }
   })
   .done(function(result) {
