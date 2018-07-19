814daad9904e07aa2a8a35177e4679e6fa90c61f
undefinito <hindakosned@gmail.com>
2018-07-19 16:58:20 +0800

    Removed lazy loading of horizontal list
    Scrapped lazy loading of the horizontal lists and just loaded all users
    before page load
    Also added delete functionality

--- jquery_only/static/js/page.js
+++ jquery_only/static/js/page.js
@@ -8,9 +8,23 @@ var PER_PAGE_V = 7
 var PER_PAGE_H = 5
 var current_page_v = 0
 
-// list is loading flags (to prevent duplicates)
+// list is loading flag (to prevent duplicates)
 var v_loading = false
-var h_loading = false
+
+function getUsers() {
+
+  $.ajax({
+    url: HOST+'/api/users',
+    method: 'GET',
+    dataType: 'json',
+    data: {
+      per_page: user_total
+    }
+  })
+  .done(function(result) {
+    users = result.data
+  })
+}
 
 function formatToLi(data) {
 
@@ -25,9 +39,10 @@ function formatToLi(data) {
     var $t_card = $card.clone()
     $t_card.find('[data-avatar]').attr('src',users[i].avatar)
     $t_card.find('[data-name]').text(users[i].first_name + ' ' + users[i].last_name)
-    $t_card.find('[data-job]').text(users[i].job)
+    $t_card.find('[data-toggle]').attr('data-id',users[i].id)
 
     users_html += $t_card.get(0).outerHTML
+
   }
 
   // generate list item html
@@ -94,6 +109,7 @@ function renderVList(data,is_append,cb) {
 // GET users (for horizontal list)
 $.ajax({
   url: HOST+'/api/users',
+  async: false,
   method: 'GET',
   dataType: 'json',
   data: {
@@ -101,9 +117,11 @@ $.ajax({
   }
 })
 .done(function(result) {
-  users = result.data
   user_total = result.total
+  // get all users
+  getUsers()
 })
+
 
 // GET horizontal list item html
 $.get('html/user_card.html')
@@ -112,6 +130,7 @@ $.get('html/user_card.html')
   })
   .fail(function() {
     // TODO handler of error
+    $('[data-alert="danger"]').removeClass('d-none')
   })
 
 // GET list item html
@@ -121,6 +140,7 @@ $.get('html/list_item.html')
   })
   .fail(function() {
     // TODO handler of error
+    $('[data-alert="danger"]').removeClass('d-none')
   })
 
 $(document).ready(function() {
@@ -128,8 +148,8 @@ $(document).ready(function() {
   // get initial page load (items per page dependent on PER_PAGE_V)
   renderVList({per_page: PER_PAGE_V})
 
+  // lazy loading of vertical list
   $(document).scroll(function(){
-    console.log('list scroll')
 
     // if at the bottom of the page
     if(($(window).scrollTop() + $(window).height() + 50) >= $(document).height()) {
@@ -142,6 +162,7 @@ $(document).ready(function() {
       }
     }
   })
+
 
   $('#add_modal').on('hide.bs.modal', function() {
     // clear all inputs
@@ -216,4 +237,112 @@ $(document).ready(function() {
     })
   })
 
+  // user info
+  $('.main.card').on('click', '[data-users] [data-info]', function(){
+    var id = $(this).attr('data-id')
+
+    console.log('id:',id)
+
+    $.ajax({
+      url: HOST+'/api/users/'+id,
+      method: 'GET',
+      dataType: 'json',
+      beforeSend: function() {
+        // show loading
+        $('[data-loading="info"]').removeClass('d-none')
+      }
+    })
+    .done(function(result) {
+      var data = result.data
+      // show details
+      $('[data-details]').removeClass('d-none')
+        .find('[data-avatar]').attr('src',data.avatar).end()
+        .find('[data-firstname]').text(data.first_name).end()
+        .find('[data-lastname]').text(data.last_name).end()
+        .find('[data-id]').attr('data-id',data.id).end()
+      // hide loading
+      $('[data-loading="info"]').addClass('d-none')
+    })
+  })
+
+  // delete user 
+  $('[data-delete]').on('click', function() {
+    // hide button
+    $(this).addClass('d-none')
+    // show confirmation message
+    $('[data-confirm]').removeClass('d-none')
+  })
+
+  // confirm delete
+  $('[data-confirm]').on('click','[data-proceed]',function(){
+    var proceed = $(this).attr('data-proceed')
+    var id = $(this).attr('data-id')
+
+    console.log(id)
+    if(!id)
+    {
+      return false
+    }
+
+    switch(proceed)
+    {
+      case 'no':
+        // hide confirmation message
+        $('[data-confirm]').addClass('d-none')
+        // show delete button
+        $('[data-delete]').removeClass('d-none')
+        break
+
+      // proceed with deletion
+      case 'yes':
+        $.ajax({
+          url: HOST+'/api/users/'+id,
+          method: 'DELETE',
+          dataType: 'json',
+          beforeSend: function() {
+            $('#info_modal').find('[data-dismiss="modal"]').addClass('d-none')
+          }
+        })
+        .always(function(a,b,c){
+
+          // hide confirmation message
+          $('[data-confirm]').addClass('d-none')
+          
+          // delete success
+          if(c.status == 204)
+          {
+            // show message
+            $('[data-done]').removeClass('d-none')
+            setTimeout(function(){
+              // show dismiss modal button
+              $('#info_modal').find('[data-dismiss="modal"]').removeClass('d-none')
+              // hide modal
+              $('#info_modal').modal('hide')
+            },1500)
+          }
+          else
+          {
+            // delete failed
+
+            // show dismiss modal button
+            $('#info_modal').find('[data-dismiss="modal"]').removeClass('d-none')
+
+            // show delete button
+            $('[data-delete]').removeClass('d-none')
+          }
+        })
+        break
+    }
+  })
+
+  $('#info_modal').on('hide.bs.modal', function() {
+    // hide info
+    $('[data-details]').addClass('d-none')
+    // show loading
+    $('[data-loading="info"]').removeClass('d-none')
+    // show delete button
+    $('[data-delete]').removeClass('d-none')
+    // hide message
+    $('[data-done]').addClass('d-none')
+  })
 })
